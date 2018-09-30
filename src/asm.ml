f9608fd4a65315db38f25a014667511da4b01fab
uenoku <uenoku.tokotoko@gmail.com>
2018-10-01 01:27:53 +0900

    format

--- src/asm.ml
+++ src/asm.ml
@@ -1,18 +1,20 @@
 type reg = int
-type iop = Mul | Add | Sub | Div 
-type fop = Mul | Add | Sub | Div 
+
+type iop = Mul | Add | Sub | Div
+
+type fop = Mul | Add | Sub | Div
 
 type 'a t =
-| Op of iop * reg * reg * reg * 'a 
-| Opi of iop * reg * reg * int * 'a 
-| FOp of fop * reg * reg * reg * 'a 
-| FOpi of fop * reg * reg * float * 'a 
-| Label of string * 'a 
-| Load of reg * reg * int * 'a
-| Store of reg * reg * int * 'a
-| Cmpd of reg * reg * 'a
-| BEQ of reg * reg * 'a
-| BLE of reg * reg * 'a
-| Jump of string * 'a
-| In of reg * 'a 
-| Out of reg * 'a
+  | Op of iop * reg * reg * reg * 'a
+  | Opi of iop * reg * reg * int * 'a
+  | FOp of fop * reg * reg * reg * 'a
+  | FOpi of fop * reg * reg * float * 'a
+  | Label of string * 'a
+  | Load of reg * reg * int * 'a
+  | Store of reg * reg * int * 'a
+  | Cmpd of reg * reg * 'a
+  | BEQ of reg * reg * 'a
+  | BLE of reg * reg * 'a
+  | Jump of string * 'a
+  | In of reg * 'a
+  | Out of reg * 'a
--- src/virtual.ml
+++ src/virtual.ml
@@ -1,56 +1,50 @@
 open Syntax
+
 type label = string
+
 type 'a u =
-| Nop of 'a
-| Li of int * 'a
-| FLi of float * 'a
-| Mr of var * 'a
-| FMr of var * 'a
-| Op of op * (var list) * 'a
-| Load of var * var  * 'a
-| Store of var * var * var * 'a
-| IfEq of var * var * 'a v * 'a v * 'a
-| IfLE of var * var * 'a v * 'a v * 'a
-| CallCls of var * var list * var list * 'a
-| CallDir of label * var list * var list * 'a
-| Save of var * var * 'a
-| Restore of var  * 'a
+  | Nop of 'a
+  | Li of int * 'a
+  | FLi of float * 'a
+  | Mr of var * 'a
+  | FMr of var * 'a
+  | Op of op * var list * 'a
+  | Load of var * var * 'a
+  | Store of var * var * var * 'a
+  | IfEq of var * var * 'a v * 'a v * 'a
+  | IfLE of var * var * 'a v * 'a v * 'a
+  | CallCls of var * var list * var list * 'a
+  | CallDir of label * var list * var list * 'a
+  | Save of var * var * 'a
+  | Restore of var * 'a
 
-and 'a v = 
-| Ans of 'a u
-| Let of var * 'a u * 'a v
-let rec concat  var e1 e2= 
-match e1 with
-| Ans(x) -> 
-    Let(var, x, e2)
-| Let(y,t1,t2) -> 
-    Let(y, t1, concat var t2 e2 )
+and 'a v = Ans of 'a u | Let of var * 'a u * 'a v
 
-let rec closure_to_virtual (e:debug Knormal.u) = match e with
-| Const(CUnit,d) -> Ans(Nop(d))
-| Const(CInt(x),d) -> Ans(Li(x,d))
-| Const(CFloat(x),d) -> Ans(FLi(x,d))
-| Op(op,l,d) -> 
-    Ans(Op(op, l,d))
-| If(EQ, x,y,e1,e2,d) -> 
-(
+let rec concat var e1 e2 =
+  match e1 with
+  | Ans x -> Let (var, x, e2)
+  | Let (y, t1, t2) -> Let (y, t1, concat var t2 e2)
+
+let rec closure_to_virtual (e: debug Knormal.u) =
+  match e with
+  | Const (CUnit, d) -> Ans (Nop d)
+  | Const (CInt x, d) -> Ans (Li (x, d))
+  | Const (CFloat x, d) -> Ans (FLi (x, d))
+  | Op (op, l, d) -> Ans (Op (op, l, d))
+  | If (EQ, x, y, e1, e2, d) -> (
     match x.ty with
-    | Type.TyBool | Type.TyInt  -> Ans(IfEq(x,y, closure_to_virtual e1, closure_to_virtual e2,d))
-    | _ -> failwith "equality is only bool and int"
-)
-    
-| If(LE, x,y,e1,e2,d) -> 
-(
+    | Type.TyBool | Type.TyInt ->
+        Ans (IfEq (x, y, closure_to_virtual e1, closure_to_virtual e2, d))
+    | _ -> failwith "equality is only bool and int" )
+  | If (LE, x, y, e1, e2, d) -> (
     match x.ty with
-    | Type.TyBool | Type.TyInt  -> Ans(IfLE(x,y, closure_to_virtual e1, closure_to_virtual e2,d))
-    | _ -> failwith "equality is only bool and int"
-)
-| Let(var, e1, e2,d) ->
-    concat var (closure_to_virtual e1) (closure_to_virtual e2)
-| Var(var,d) -> 
-    (
-        match var.ty with
-        | Type.TyUnit -> Ans(Nop(d))
-        | Type.TyFloat -> Ans(FMr(var,d))
-        | _ -> Ans(Mr(var,d))
-    )
+    | Type.TyBool | Type.TyInt ->
+        Ans (IfLE (x, y, closure_to_virtual e1, closure_to_virtual e2, d))
+    | _ -> failwith "equality is only bool and int" )
+  | Let (var, e1, e2, d) ->
+      concat var (closure_to_virtual e1) (closure_to_virtual e2)
+  | Var (var, d) ->
+    match var.ty with
+    | Type.TyUnit -> Ans (Nop d)
+    | Type.TyFloat -> Ans (FMr (var, d))
+    | _ -> Ans (Mr (var, d))
