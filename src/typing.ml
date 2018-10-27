1413033c0eb0af926072736ad140c667e59c98e1
uenoku <uenoku.tokotoko@gmail.com>
2018-10-27 20:27:38 +0900

    murininatta

--- src/typing.ml
+++ src/typing.ml
@@ -10,19 +10,37 @@ exception UnifyError of string * string
 
 let empty = []
 
-let get_eq l r d = {left= l; right= r; debug= d}
+let get_eq l r d =
+    match l, r with
+    | TyInt  ,TyInt 
+    | TyFloat ,TyFloat
+    | TyUnit,TyUnit
+    | TyBool,TyBool   -> []
+    | _ -> 
+            [ {left= l; right= r; debug= d}]
 
-let rec ty_subst (sigma : (int * t) list) t =
+module StringMap = Map.Make (struct
+  type t = string
+
+  let compare = compare 
+end)
+
+module IntMap = Map.Make (struct
+  type t = int
+
+  let compare = compare 
+end)
+
+let rec ty_subst sigma t =
   match t with
   | (TyInt | TyUnit | TyBool | TyFloat) as self -> self
   | TyFun (a, b) -> TyFun (List.map (ty_subst sigma) a, ty_subst sigma b)
   | TyVar alpha ->
-      let rec find l =
-        match l with
-        | [] -> TyVar alpha
-        | (a, b) :: ls -> if a = alpha then b else find ls
-      in
-      find sigma
+          (
+      match IntMap.find_opt alpha sigma with
+      | Some(x) -> x
+      | None -> TyVar alpha
+      )
   | TyTuple a -> TyTuple (List.map (ty_subst sigma) a)
   | TyArray ty -> TyArray (ty_subst sigma ty)
 
@@ -30,17 +48,17 @@ let subst_equations tyvar ty equations =
   List.map
     (fun x ->
       { x with
-        left= ty_subst [(tyvar, ty)] x.left
-      ; right= ty_subst [(tyvar, ty)] x.right } )
+        left= ty_subst (IntMap.add tyvar ty IntMap.empty) x.left
+      ; right= ty_subst (IntMap.add tyvar ty IntMap.empty) x.right } )
     equations
 
-let rec compose (sigma2 : (int * Type.t) list) (sigma1 : (int * Type.t) list) =
-  let sigma11 = List.map (fun (tx, t) -> (tx, ty_subst sigma2 t)) sigma1 in
-  List.fold_left
-    (fun tau (tx, t) ->
-      if List.exists (fun (x, y) -> x = tx) sigma1 then tau else (tx, t) :: tau
+let rec compose sigma2 sigma1 =
+  let sigma11 = IntMap.fold (fun tx t acc -> IntMap.add tx (ty_subst sigma2 t) acc )sigma1  IntMap.empty  in
+  IntMap.fold 
+    (fun tx t tau ->
+      if IntMap.mem tx sigma1 then tau else IntMap.add tx t tau
       )
-    sigma11 sigma2
+     sigma2 sigma11
 
 let rec occur a t =
   match t with
@@ -55,23 +73,23 @@ let same_len l1 l2 eq =
     raise (UnifyError (show_type_equation eq, "should have same length"))
   else ()
 
-let rec (ty_unify : type_equation list -> (int * Type.t) list) =
+let rec (ty_unify: type_equation list -> Type.t IntMap.t) =
  fun equations ->
   match equations with
-  | [] -> []
-  | eq :: rest -> (
+  | [] -> IntMap.empty
+  | eq :: rest ->
     match (eq.left, eq.right) with
     | TyVar a, TyVar b ->
         if a = b then ty_unify rest
         else
-          compose (ty_unify (subst_equations a eq.right rest)) [(a, eq.right)]
+          compose (ty_unify (subst_equations a eq.right rest)) (IntMap.singleton a eq.right)
     | TyFun (s, t), TyFun (s', t') ->
         let _ = same_len s s' in
-        let args = List.map2 (fun x y -> get_eq x y eq.debug) s s' in
-        ty_unify (args @ [get_eq t t' eq.debug] @ rest)
+        let args = List.concat (List.map2 (fun x y -> get_eq x y eq.debug) s s') in
+        ty_unify (args @ (get_eq t t' eq.debug) @ rest)
     | TyTuple s, TyTuple s' ->
         let _ = same_len s s' in
-        let args = List.map2 (fun x y -> get_eq x y eq.debug) s s' in
+        let args = List.concat (List.map2 (fun x y -> get_eq x y eq.debug) s s') in
         ty_unify (args @ rest)
     | TyBool, TyBool | TyUnit, TyUnit | TyFloat, TyFloat | TyInt, TyInt ->
         ty_unify rest
@@ -79,13 +97,13 @@ let rec (ty_unify : type_equation list -> (int * Type.t) list) =
         if occur a eq.right then
           raise (UnifyError (show_type_equation eq, "occurence check"))
         else
-          compose (ty_unify (subst_equations a eq.right rest)) [(a, eq.right)]
+          compose (ty_unify (subst_equations a eq.right rest)) (IntMap.singleton a eq.right)
     | _, TyVar a ->
         if occur a eq.left then
           raise (UnifyError (show_type_equation eq, "occurence check"))
-        else compose (ty_unify (subst_equations a eq.left rest)) [(a, eq.left)]
-    | TyArray a, TyArray b -> ty_unify (get_eq a b eq.debug :: rest)
-    | _ -> raise (UnifyError (show_type_equation eq, "unify failed")) )
+        else compose (ty_unify (subst_equations a eq.left rest)) (IntMap.singleton a eq.left)
+    | TyArray a, TyArray b -> ty_unify ((get_eq a b eq.debug) @ rest)
+    | _ -> raise (UnifyError (show_type_equation eq, "unify failed"))
 
 let builtin_function' =
   [ ("print_int", ([], [TyInt], TyUnit))
@@ -118,6 +136,8 @@ let builtin_function' =
   ; ("atan", ([], [TyFloat], TyFloat))
   ; ("sqrt", ([], [TyFloat], TyFloat))
   ; ("invsqrt", ([], [TyFloat], TyFloat))
+  ; ("itof", ([], [TyInt], TyFloat))
+  ; ("ftoi", ([], [TyFloat], TyInt))
   ; ("float_of_int", ([], [TyInt], TyFloat))
   ; ("int_of_float", ([], [TyFloat], TyInt))
   ; ( "create_array"
@@ -130,7 +150,7 @@ let builtin_vars =
     builtin_function'
 
 let instanciate_builtin name varenv =
-  match name with
+  match name.name with
   | "create_array" ->
       let s = Type.genvar () in
       TyFun ([TyInt; TyVar s], TyArray (TyVar s))
@@ -138,21 +158,21 @@ let instanciate_builtin name varenv =
       let s = Type.genvar () in
       let t = Type.genvar () in
       TyFun ([TyVar s], TyVar t)
-  | _ -> (
+  | _ ->
     try
-      let a, b, c = List.assoc name builtin_function' in
+      let a, b, c = List.assoc name.name builtin_function' in
       TyFun (b, c)
     with _ ->
-      if List.exists (fun x -> x.name = name) varenv then TyInt
-      else raise (TypingError ("unbound " ^ name)) )
+      if VarSet.mem name varenv then TyInt
+      else raise (TypingError ("unbound " ^ name.name))
 
 let subst_var sigma v varenv =
   let (TyVar tyvar) = v.ty in
-  match List.find_opt (fun (x, y) -> x = tyvar) sigma with
-  | Some (var, ty) -> {v with ty}
+  match IntMap.find_opt tyvar sigma with
+  | Some ( ty) -> {v with ty = ty}
   | None ->
-      let ty = instanciate_builtin v.name varenv in
-      {v with ty}
+      let ty = instanciate_builtin v varenv in
+      {v with ty = ty}
 
 let rec gather_eq' type_env e =
   let gather_eq = gather_eq' type_env in
@@ -170,23 +190,23 @@ let rec gather_eq' type_env e =
           in
           let t = f all in
           let beta = List.nth t idx in
-          (beta, get_eq ty beta d :: get_eq (TyTuple t) t1 d :: c1)
+          (beta, (get_eq ty beta d ) @ (get_eq (TyTuple t) t1 d) @ c1)
       | TyTuple t ->
           let beta = List.nth t idx in
-          (beta, get_eq ty beta d :: c1)
+          (beta, get_eq ty beta d @ c1)
       | _ -> raise (TypingError ("type error tuple at " ^ show_debug d)) )
   | Op (ArrayGet alpha, [arr; idx], d) ->
       let t1, c1 = gather_eq arr in
       let t2, c2 = gather_eq idx in
-      (alpha, get_eq t1 (TyArray alpha) d :: get_eq t2 TyInt d :: (c1 @ c2))
+      (alpha, (get_eq t1 (TyArray alpha) d) @ (get_eq t2 TyInt d ) @ (c1 @ c2))
   | Op (ArrayPut alpha, [arr; idx; elem], d) ->
       let t1, c1 = gather_eq arr in
       let t2, c2 = gather_eq idx in
       let t3, c3 = gather_eq elem in
       ( TyUnit
       , get_eq t1 (TyArray alpha) d
-        :: get_eq t2 TyInt d :: get_eq t3 alpha d
-        :: (c1 @ c2 @ c3) )
+        @ get_eq t2 TyInt d @ get_eq t3 alpha d
+        @ (c1 @ c2 @ c3) )
   | Op (Primitive x, l, d) ->
       let info = get_o_info x in
       let res = List.map gather_eq l in
@@ -200,42 +220,49 @@ let rec gather_eq' type_env e =
       let t1, c1 = gather_eq e1 in
       let t2, c2 = gather_eq e2 in
       let t3, c3 = gather_eq e3 in
-      (t2, get_eq TyBool t1 d :: get_eq t2 t3 d :: List.concat [c1; c2; c3])
+      (t2, get_eq TyBool t1 d @ get_eq t2 t3 d @ List.concat [c1; c2; c3])
   | LetRec (fundef, e1, d) ->
       let alpha = fundef.f.ty in
       let args_var = List.map (fun x -> x.ty) fundef.args in
       let beta = TyVar (Type.genvar ()) in
       let ty = TyFun (args_var, beta) in
+      let tmp = List.map (fun x -> (x, x.ty)) fundef.args
+          @ [(fundef.f, ty)] in
+      let newtypeenv = List.fold_left (fun acc (p,ty) -> 
+        VarMap.add p ty acc 
+      ) type_env tmp in
       let t1, c1 =
         gather_eq'
-          ( List.map (fun x -> (x.name, x.ty)) fundef.args
-          @ [(fundef.f.name, ty)] @ type_env )
+         newtypeenv
           fundef.body
       in
-      let t2, c2 = gather_eq' ((fundef.f.name, ty) :: type_env) e1 in
-      (t2, get_eq ty alpha d :: get_eq t1 beta d :: (c1 @ c2))
+      let t2, c2 = gather_eq' (VarMap.add  fundef.f  ty  type_env) e1 in
+      (t2, get_eq ty alpha d @ get_eq t1 beta d @ (c1 @ c2))
       (* (t2, get_eq ty alpha d :: (c1 @ c2)) *)
   | Let (var, e1, e2, d) ->
       let t1, c1 = gather_eq e1 in
       (*
             let多相はあとからやる
         *)
-      let env = (var.name, var.ty) :: type_env in
+      (* let env = (var.name, var.ty) :: type_env in *)
+      let env = VarMap.add var var.ty type_env in
       (*
          あとのことを考えて
         *)
       let t2, c2 = gather_eq' env e2 in
-      (t2, (get_eq var.ty t1 d :: c1) @ c2)
+      (t2, (get_eq var.ty t1 d @ c1) @ c2)
   | Var (name, d) -> (
-    (*
+    match
+      (*
         globalでinstanciate は実装する必要がある
         create_arrayの第二引数は多相になってるから
         *)
-    match List.find_opt (fun (x, y) -> x = name.name) type_env with
-    | Some (str, ty) -> (ty, [get_eq ty name.ty d])
+      VarMap.find_opt name type_env
+    with
+    | Some (ty) -> (ty, get_eq ty name.ty d)
     | None ->
-        let ty = instanciate_builtin name.name [] in
-        (ty, [get_eq ty name.ty d]) )
+        let ty = instanciate_builtin name VarSet.empty in
+        (ty, get_eq ty name.ty d) )
   | Tuple (ts, d) ->
       let res = List.map gather_eq ts in
       let types = List.map fst res in
@@ -247,7 +274,7 @@ let rec gather_eq' type_env e =
       let ts = List.map fst res in
       let cs = List.concat (List.map snd res) in
       let alpha = TyVar (Type.genvar ()) in
-      (alpha, get_eq (TyFun (ts, alpha)) t1 d :: (c1 @ cs))
+      (alpha, get_eq (TyFun (ts, alpha)) t1 d @ (c1 @ cs))
 
 let rec subst_ast sigma varenv s =
   let f = subst_ast sigma varenv in
@@ -261,7 +288,7 @@ let rec subst_ast sigma varenv s =
   | Op (op, ts, d) -> Op (op, List.map f ts, d)
   | If (t0, t1, t2, d) -> If (f t0, f t1, f t2, d)
   | Let (var, t0, t1, d) ->
-      let k = subst_ast sigma (var :: varenv) in
+      let k = subst_ast sigma (VarSet.add var varenv) in
       Let (subst_var sigma var varenv, f t0, k t1, d)
   | Var (var, d) -> Var (subst_var sigma var varenv, d)
   | LetRec (fundef, t0, d) ->
@@ -270,17 +297,18 @@ let rec subst_ast sigma varenv s =
             f= subst_var sigma fundef.f varenv
           ; args= List.map (fun x -> subst_var sigma x varenv) fundef.args
           ; body=
-              subst_ast sigma ((fundef.f :: fundef.args) @ varenv) fundef.body
+              subst_ast sigma (VarSet.union (VarSet.of_list (fundef.f :: fundef.args))  varenv) fundef.body
           }
-        , subst_ast sigma (fundef.f :: varenv) t0
+        , subst_ast sigma (VarSet.add  fundef.f varenv) t0
         , d )
   | App (t0, ts, d) -> App (f t0, List.map f ts, d)
   | Tuple (ts, d) -> Tuple (List.map f ts, d)
 
 let f e =
-  let ty, equations = gather_eq' [] e in
+  let ty, equations = gather_eq' VarMap.empty e in
+
   let _ = print_string "get_equations\n" in
   let sigma = ty_unify equations in
   let _ = print_string "get_subst\n" in
-  let e = subst_ast sigma [] e in
+  let e = subst_ast sigma VarSet.empty e in
   e
