b7ea43045e3287b144bcfb091c89878503008256
uenoku <uenoku.tokotoko@gmail.com>
2018-10-29 17:58:11 +0900

    change main

--- src/main.ml
+++ src/main.ml
@@ -41,6 +41,7 @@ let lexbuf oc l =
   let _ = print_string "\nanormalized\n" in
   let _ = if !show_anormal then Knormal.myprint p 0 else () in
   let p = LambdaLifting.f p in
+  let p = Alpha.f p in
   let _ = if !show_afeter_lambda_lifting then Knormal.myprint p 0 else () in
   let p = optimtime 50 p in
   let _ = print_string "\noptimized\n" in
